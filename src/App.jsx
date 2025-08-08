b738bccbcd59db27c3421f64d1c2dead8155c361
Luiz Fernando <luizfranca35mc@gmail.com>
2025-08-07 21:55:13 -0300

    Criando mais componentes, Titulo, Rodapé...
    Aos poucos criando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Titulo from './components/Titulo';
+import Rodape from './components/rodape';
 
 function App() {
  
@@ -11,6 +12,7 @@ function App() {
       <div className='container'>
         <Titulo />
       </div>
+      <Rodape />
       
     </>
   )
---
+++ src/components/Rodape.jsx
@@ -0,0 +1,19 @@
+import './Rodape.css'
+
+function Rodape() {
+    return (
+        <>
+<footer className='text-center bg-dark text-white p-3'>
+<p>
+    Desenvolvido por &nbsp;
+    <a href="https://www.linkedin.com/in/luiz-fernando-328256249/" target='blank' className='text-danger'>Luiz Fernando</a>
+    &nbsp; no &nbsp;
+    <a href="https://www.sp.senac.br/senac-americana" target='blank' className='text-danger'>Senac Americana</a>
+    
+</p>
+</footer>
+
+        </>
+    )
+}
+export default Rodape
